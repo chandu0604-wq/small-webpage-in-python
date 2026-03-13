d38d3c692524353b38943ebea7aaf1de5b67cdd8
Hruthik D Gowda <chandud961@gmail.com>
2026-03-13 23:10:57 +0530

    Remove commented-out Flask app code and HTML

--- app.py
+++ app.py
@@ -10,7 +10,7 @@
 
 # <head>
 
-# <title>Zero Dashboard</title>
+# <title>Web in python</title>
 
 # <!-- Bootstrap CSS -->
 # <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
